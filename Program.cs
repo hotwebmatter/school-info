ece77ede08c81b5b216885a945dba3a59c61b7ec
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-17 16:14:05 -0500

    Implement algorithm per spec

--- Program.cs
+++ Program.cs
@@ -7,7 +7,14 @@ namespace school_info
     {
         static void Main(string[] args)
         {
-            WriteLine(WriteStars(42));
+            string schoolName = "Code Academy",
+                   schoolColors = "#63B1E5 (Pantone 292)";
+            int studentPopulation = 1000000;
+            WriteLine(WriteStars(72));
+            WriteLine(WriteInfo("School name:", schoolName));
+            WriteLine(WriteInfo("No. of students:", String.Format("{0:N0}", studentPopulation)));
+            WriteLine(WriteInfo("School colors:", schoolColors));
+            WriteLine(WriteStars(72));
         }
 
         static string WriteStars(int reps)
@@ -17,6 +24,16 @@ namespace school_info
             {
                 result += String.Format("*");
             }
+            return result;
+        }
+
+        static string WriteInfo(string label, string value)
+        {
+            string result = string.Empty;
+            result += WriteStars(2);
+            result += String.Format("{0, 32} {1, 32}", label, value);
+            result += "   ";
+            result += WriteStars(2);
             return result;
         }
     }
