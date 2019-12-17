30156cf81375baaabfd103ed487264c9cdd339f5
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-17 15:48:30 -0500

    Initial commit of school-info project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace school_info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
